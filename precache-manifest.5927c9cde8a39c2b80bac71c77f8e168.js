0242ed3ec56f409bc45cded5747bb9a785d3f4c6
unknown <marcoscianziogamero@gmail.com>
2020-11-03 04:45:42 -0300

    Updates

---
+++ precache-manifest.5927c9cde8a39c2b80bac71c77f8e168.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d55fee5735bfdcc5b005382af7ade6e",
+    "url": "/sharktech/index.html"
+  },
+  {
+    "revision": "3cafc6a24cfcc5518e90",
+    "url": "/sharktech/static/css/2.76715e59.chunk.css"
+  },
+  {
+    "revision": "7bb6f41ffa00209479b5",
+    "url": "/sharktech/static/css/main.2184cbd5.chunk.css"
+  },
+  {
+    "revision": "3cafc6a24cfcc5518e90",
+    "url": "/sharktech/static/js/2.915f58b7.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/sharktech/static/js/2.915f58b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bb6f41ffa00209479b5",
+    "url": "/sharktech/static/js/main.e3c0f029.chunk.js"
+  },
+  {
+    "revision": "535b414b18514d14159b",
+    "url": "/sharktech/static/js/runtime-main.f7906d08.js"
+  },
+  {
+    "revision": "f8ddc045d6be26e6483ff927933f61bf",
+    "url": "/sharktech/static/media/SSD.f8ddc045.png"
+  },
+  {
+    "revision": "f7d7f03c065ea6324cd3c4d629325a1f",
+    "url": "/sharktech/static/media/auricular.f7d7f03c.png"
+  },
+  {
+    "revision": "33362528356e8ca7a982f396c73f022c",
+    "url": "/sharktech/static/media/cpu.33362528.png"
+  },
+  {
+    "revision": "dca1133466d3c7b6d31e2947800ebeed",
+    "url": "/sharktech/static/media/discoduro.dca11334.png"
+  },
+  {
+    "revision": "daae9472ad2c616f8c52b2c3acd86fb2",
+    "url": "/sharktech/static/media/diseño.daae9472.svg"
+  },
+  {
+    "revision": "3839ca88aa47913dedd18dde6da0a9da",
+    "url": "/sharktech/static/media/gpu.3839ca88.png"
+  },
+  {
+    "revision": "b34d9c88c90cc8dc25ce5a88b5c338aa",
+    "url": "/sharktech/static/media/microfono.b34d9c88.png"
+  },
+  {
+    "revision": "6076c72d16f9f7ae6da02b9e6743461a",
+    "url": "/sharktech/static/media/mockup.6076c72d.svg"
+  },
+  {
+    "revision": "07c8f0529da433d293e6ebb3e674032b",
+    "url": "/sharktech/static/media/monitor.07c8f052.png"
+  },
+  {
+    "revision": "76ebd980e2efa59eefbb526fcf61ea3e",
+    "url": "/sharktech/static/media/mouse.76ebd980.png"
+  },
+  {
+    "revision": "ec6f4c28a2d34d92884cd7dda23fce60",
+    "url": "/sharktech/static/media/pagos.ec6f4c28.svg"
+  },
+  {
+    "revision": "0017ed2cdddbaf51d90e2acfe4f372d9",
+    "url": "/sharktech/static/media/perfil1.0017ed2c.jpg"
+  },
+  {
+    "revision": "fc872bfcd9e66de0fd26331bfa9cd9c2",
+    "url": "/sharktech/static/media/perfil2.fc872bfc.jpg"
+  },
+  {
+    "revision": "bd8198f675bd7f1afb2f4e8d8179b6f6",
+    "url": "/sharktech/static/media/perfil3.bd8198f6.jpg"
+  },
+  {
+    "revision": "309cbbcebe5025bf83a3a482de9825d2",
+    "url": "/sharktech/static/media/placamadre.309cbbce.png"
+  },
+  {
+    "revision": "58f5efd238c8663449de456dfe0b2bb5",
+    "url": "/sharktech/static/media/ram.58f5efd2.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/sharktech/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/sharktech/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/sharktech/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/sharktech/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "a6cdb4171406c254ebc2c47fec04ebdc",
+    "url": "/sharktech/static/media/teclado.a6cdb417.png"
+  }
+]);
